5a1a8d9e405327e428ec630b03551ba6c954c2e7
v.sheremetov <v.sheremetov@b2b-center.ru>
2017-06-22 20:28:56 +0300

    main file rename

--- examples/byteking-service.js
+++ examples/byteking-service.js
@@ -1,5 +1,7 @@
 const path = require('path');
-const ByteKingAgent = require(path.join(__dirname, '../byteking-agent'));
+const ByteKingAgent = require(path.join(__dirname, '../index'));
+// use code below on deploy
+// const ByteKingAgent = require('byteking-agent');
 
 const bk = new ByteKingAgent();
 bk.init(path.join(__dirname, './custom_config.json'));
